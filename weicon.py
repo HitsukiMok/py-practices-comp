7454278270fd1b7856397e5f26e5948c6b42e821
Hizu <bandorifordawin20@gmail.com>
2026-03-18 00:00:46 +0800

    Weight Converter: V2

--- weicon.py
+++ weicon.py
@@ -4,21 +4,35 @@ import sys
 
 CONVERT_VALUE = 2.205
 
-try:
-    weight = float(input("Enter your weight: "))
-except ValueError:
-    print("Enter a valid value (Numeric)")
-    sys.exit(0)
+
+while True:
+
+    try:
+        weight = float(input("Enter your weight: "))
+    except ValueError:
+        print("Enter a valid value (Numeric)")
+        sys.exit(0)
+
+    #Implemeted an .upper function here to avoid case sensitivity.
+    userInput = input("Is the number above in (K)g or (L)bs (K for Kg, L for Lbs): ").upper()
+
+    if userInput == "K":
+        converted = weight * CONVERT_VALUE
+        print(f"You weigh {converted:.2f} lbs.")
+    elif userInput == "L":
+        converted = weight / CONVERT_VALUE
+        print(f"You weigh {converted:.2f} kg.")
+    else:
+        print("Please enter a valid option.")
+
+    try:
+        cont = int(input("Do you want to try again? \n0 - Yes \n1 - No \n>>"))
+    except ValueError:
+        print("Unidentified. Please answer properly.")
+        sys.exit(0)
+
+    if cont == 1:
+        break
 
 
-#Implemeted an .upper function here to avoid case sensitivity.
-userInput = input("Is the number above in (K)g or (L)bs (K for Kg, L for Lbs): ").upper()
-
-if userInput == "K":
-    converted = weight * CONVERT_VALUE
-    print(f"You weigh {converted:.2f} lbs.")
-elif userInput == "L":
-    converted = weight / CONVERT_VALUE
-    print(f"You weigh {converted:.2f} kg.")
-else:
-    print("Please enter a valid option.")
+print("Exiting program...")
